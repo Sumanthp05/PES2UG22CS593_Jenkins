a6c5dfd3f5ed697a3e8e5199e6c2a6a74b6e507b
Sumanthp05 <sumanthp05@gmail.com>
2025-03-12 22:35:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout<< "PES2UG22CS593 \n";
   return 0;
-  cout<<"PES2UG22CS593\n";
 }
